d2e46e575d691660e04a3d6847ed67393f9a958b
Alejandra Pardo <maria.alejandra.pardo@utah.edu>
2018-04-21 15:31:31 -0600

    h1 to h5 attempt

--- pa3_switch.py
+++ pa3_switch.py
@@ -97,6 +97,11 @@ class PA3Switch(app_manager.RyuApp):
                 if robin_value%2 ==  1:
                 	print('sending arp reply with 10.0.0.5 ')
 
+        			match = parser.OFPMatch(in_port=in_port, ipv4_dst="10.0.0.10", eth_dst=dst, eth_src=src)
+        			actions = [parser.OFPActionOutput(5)]
+        			self.add_flow(datapath, 1, match, actions)
+
+
 
         if eth.ethertype == ether_types.ETH_TYPE_LLDP:
             # ignore lldp packet
@@ -112,14 +117,14 @@ class PA3Switch(app_manager.RyuApp):
 
         # learn a mac address to avoid FLOOD next time.
         self.mac_to_port[dpid][src] = in_port
-        print('dpid is {}'.format(dpid))
+        print('adding {} {} {}'.format(dpid, src, inport))
 
-        if dst in self.mac_to_port[dpid]:
-            out_port = self.mac_to_port[dpid][dst]
-        else:
-            out_port = ofproto.OFPP_FLOOD
+        #if dst in self.mac_to_port[dpid]:
+         #   out_port = self.mac_to_port[dpid][dst]
+        #else:
+         #   out_port = ofproto.OFPP_FLOOD
 
-        actions = [parser.OFPActionOutput(out_port)]
+        #actions = [parser.OFPActionOutput(out_port)]
 
         # install a flow to avoid packet_in next time
         if out_port != ofproto.OFPP_FLOOD:
