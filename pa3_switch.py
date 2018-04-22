5fa8d478364039a38b613942ce26b16b4ec542d2
Alejandra Pardo <maria.alejandra.pardo@utah.edu>
2018-04-21 18:52:44 -0600

    Change arp reply addresses

--- pa3_switch.py
+++ pa3_switch.py
@@ -101,23 +101,22 @@ class PA3Switch(app_manager.RyuApp):
                 print('an arp packet here!')
                 if robin_value%2 ==  1:
                 	print('sending arp reply with 10.0.0.5 ')
+                	e = ethernet.ethernet(dst='00:00:00:00:00:01', src='00:00:00:00:00:05', ethertype=ether.ETH_TYPE_ARP)
+                	a = arp.arp(hwtype=1, proto=0x0800, hlen=6, plen=4, opcode=2, src_mac='00:00:00:00:00:00', src_ip='10.0.0.5', dst_mac='00:00:00:00:00:01', dst_ip='10.0.0.1')
+                	arp_reply = packet.Packet()
+                	arp_reply.add_protocol(e)
+                	arp_reply.add_protocol(a)
+                	out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id, in_port=in_port, actions=[], data=arp_reply)
+                	datapath.send_msg(out)
+
+
                 	#robin_value = 2
+
                 else:
                 	print('sending arp reply with 10.0.0.6 ')
                 	#robin_value = 1
         
-        e = ethernet.ethernet(dst='ff:ff:ff:ff:ff:ff',
-                      src='08:60:6e:7f:74:e7',
-                      ethertype=ether.ETH_TYPE_ARP)
-        a = arp.arp(hwtype=1, proto=0x0800, hlen=6, plen=4, opcode=2,
-            src_mac='08:60:6e:7f:74:e7', src_ip='192.0.2.1',
-            dst_mac='00:00:00:00:00:00', dst_ip='192.0.2.2')
-        arp_reply = packet.Packet()
-        arp_reply.add_protocol(e)
-        arp_reply.add_protocol(a)
-        #arp_reply.serialize()
-        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id, in_port=in_port, actions=[], data=arp_reply)
-        datapath.send_msg(out)
+
 
 
         if eth.ethertype == ether_types.ETH_TYPE_LLDP:
